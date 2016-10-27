c7b817954d74d12db943f4a212654173b283c20a
sriteja1607 <sritejathuraka@sritejas-macbook-pro.local>
2016-10-27 14:07:49 -0700

    Import Alomofire and added two swift files for weather and conditions

--- ClimateApp/ViewController.swift
+++ ClimateApp/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+         print(current_Weather_Url)
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "climateCell", for: indexPath)
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
---
+++ Conditions.swift
@@ -0,0 +1,16 @@
+//
+//  Conditions.swift
+//  ClimateApp
+//
+//  Created by Sriteja Thuraka on 10/27/16.
+//  Copyright © 2016 teja. All rights reserved.
+//
+
+import Foundation
+
+let URL = "http://api.openweathermap.org/data/2.5/weather?"
+let Lat = "lat= "
+let Lon = "&lon="
+let appID = "&appid="
+let appKey = "e6e4c4f1b2ab1ba2a87b0d344815dc2a"
+let current_Weather_Url = "\(URL)\(Lat)-40\(Lon)180\(appID)\(appKey)"
