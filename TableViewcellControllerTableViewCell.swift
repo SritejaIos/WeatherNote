0c0363ac6d8d2e97c770815d749166cd4e8545fc
sriteja1607 <sritejathuraka@Sritejas-MacBook-Pro.local>
2016-11-01 16:51:40 -0700

    added table view cocoa class

---
+++ TableViewcellControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewcellControllerTableViewCell.swift
+//  ClimateApp
+//
+//  Created by Sriteja Thuraka on 11/1/16.
+//  Copyright © 2016 teja. All rights reserved.
+//
+
+import UIKit
+
+class TableViewcellControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
